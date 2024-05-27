c5664383cf8529fb09d9fb57d2195047a1f09e46
Leo0179 SDF <leonardo.leissdeleimburg@studenti.malignani.ud.it>
2024-05-27 08:38:07 +0000

    progetti in classe

---
+++ c++ project/oggi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Lorik Kalludras 192"<<endl;
+    string name;
+    cout<<"Scrivi ciao"<<endl;
+    cin>>name;
+    cout<<"Bella"<<name;
+
+
+
+    return 0;
+}
